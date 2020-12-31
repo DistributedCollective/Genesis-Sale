b19ee0ef8ff3bd74f0af2756ff25b14f64ab7359
James Howlett <jameshowlett977@gmail.com>
2020-12-31 17:52:28 +0200

    updated truffle-config.js

--- contracts/mock/NFTMockSetup.sol
+++ contracts/mock/NFTMockSetup.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.6.2;
+pragma solidity ^0.7.5;
 
 //import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 //import "openzeppelin-solidity/contracts/access/Ownable.sol";
--- contracts/mock/SovrynNft.sol
+++ contracts/mock/SovrynNft.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.6.2;
+pragma solidity ^0.7.5;
 
 //import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
 //import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
